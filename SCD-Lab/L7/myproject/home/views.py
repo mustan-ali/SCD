8dc9b4b3f1d7fe62a9a3235c52a1c62531210d1f
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-10-26 17:20:26 +0500

    added Lab-7 file.

---
+++ SCD-Lab/L7/myproject/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, "home/index.html")
+
+def about(request):
+    return render(request, "home/about.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
+
